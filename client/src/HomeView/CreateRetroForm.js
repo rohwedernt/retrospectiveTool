69bace1486180b10b1e89c96d6134814b1ebd53a
Nate Rohweder <nrohweder@ebsco.com>
2020-04-09 11:15:03 -0400

    fix: create retro (add categories) form bugs

--- client/src/HomeView/CreateRetroForm.js
+++ client/src/HomeView/CreateRetroForm.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
-import CategoryInput from './CategoryInput';
 
 const styles = {
     button: {
@@ -11,54 +12,59 @@ const styles = {
 }
 
 export default function RetroView(props) {
-    const [numOfCategories, setNumOfCategories] = useState(1);
     const [title, setTitle] = useState("");
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([{ text: null }]);
 
     const handleTitleChange = (e) => {
         setTitle(e.target.value);
     }
 
-    const setCategory = value => {
-        setCategories([...categories, value]);
+    const handleChange = (i, event) => {
+        const values = [...categories];
+        values[i].text = event.target.value;
+        setCategories(values);
     }
 
-    const addCategory = () => {
-        setNumOfCategories(numOfCategories + 1);
+    const handleAdd = () => {
+        const values = [...categories];
+        values.push({ text: null });
+        setCategories(values);
     }
 
-    const removeCategory = () => {
-        setCategories(categories.pop());
-        setNumOfCategories(numOfCategories - 1);
+    const handleRemove = (i) => {
+        const values = [...categories];
+        values.splice(i, 1);
+        setCategories(values);
     }
 
-    const renderCategoryInputs = () => {
-        let inputs = []
-        for (let i = 0; i < numOfCategories; i++) {
-            inputs.push(<CategoryInput key={i} setCategory={setCategory}/>)
-        }
-
-        return inputs
-    }
-
-	return (
+    return (
         <Fragment>
             <Modal.Header closeButton>
                 <Modal.Title>Create New Retrospective</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                {/* This form needs to gather this and send to backend and create retro item */}
                 <Form>
                     <Form.Group controlId="formBasicEmail">
                     <Form.Label>Title</Form.Label>
                         <Form.Control value={title} onChange={(e) => handleTitleChange(e)} type="text" placeholder="Enter Title" />
                     </Form.Group>
                     <Form.Label>Categories</Form.Label>
-                    {renderCategoryInputs()}
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <Button style={styles.button} variant="outline-primary" size="sm" onClick={() => addCategory()}>Add Category</Button>
-                        <Button style={styles.button} variant="outline-danger" size="sm" onClick={() => removeCategory()}>Remove Category</Button>
-                    </div>
+                    {categories.map((category, idx) => {
+                        return (
+                            <InputGroup key={`${category}-${idx}`} className="mb-3">
+                                <FormControl 
+                                    type="text" 
+                                    value={category.text} 
+                                    placeholder="Enter Category Name" 
+                                    onChange={(e) => handleChange(idx, e)} 
+                                />
+                                <InputGroup.Append>
+                                    <Button variant="outline-danger" onClick={() => handleRemove(idx)}>✘</Button>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        )}
+                    )}
+                    <Button style={styles.button} variant="outline-primary" size="sm" onClick={() => handleAdd()}>Add Category</Button>
                 </Form>
             </Modal.Body>
             <Modal.Footer>
@@ -71,5 +77,85 @@ export default function RetroView(props) {
                 </Button>
             </Modal.Footer>
         </Fragment>
-	);
+    );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const setCategory = (id, value) => {
+//         let array = [...categories];
+//         var categoryToSet = array.findIndex(x => x.id == id);
+//         array[categoryToSet].text = value;
+//         setCategories(array);
+//     }
+
+//     const addCategory = () => {
+//         setCategories([...categories, {id: categoryId++, text: ""}]);
+//     }
+
+//     const removeCategory = id => {
+//         let array = [...categories];
+//         array.splice(id, 1);
+//         setCategories(array);
+//     }
+
+//     const renderCategoryInputs = () => {
+//         let inputs = []
+//         for (let i = 0; i < categories.length; i++) {
+//             inputs.push(
+//                 <CategoryInput 
+//                     key={i} 
+//                     id={i}
+//                     setCategory={setCategory} 
+//                     removeCategory={removeCategory}
+//                     numOfCategories={categories.length} 
+//                 />
+//             )
+//         }
+
+//         return inputs
+//     }
+
+// 	return (
+//         <Fragment>
+            // <Modal.Header closeButton>
+            //     <Modal.Title>Create New Retrospective</Modal.Title>
+            // </Modal.Header>
+            // <Modal.Body>
+            //     {/* This form needs to gather this and send to backend and create retro item */}
+            //     <Form>
+            //         <Form.Group controlId="formBasicEmail">
+            //         <Form.Label>Title</Form.Label>
+            //             <Form.Control value={title} onChange={(e) => handleTitleChange(e)} type="text" placeholder="Enter Title" />
+            //         </Form.Group>
+            //         <Form.Label>Categories</Form.Label>
+            //         {renderCategoryInputs()}
+            //         <Button style={styles.button} variant="outline-primary" size="sm" onClick={() => addCategory()}>Add Category</Button>
+            //     </Form>
+            // </Modal.Body>
+            // <Modal.Footer>
+            //     <Button variant="secondary" onClick={props.handleClose}>
+            //         Cancel
+            //     </Button>
+            //     {/* this will call method to create retro on backend then render retro board of id blah */}
+            //     <Button variant="primary" onClick={() => props.changeView("retroview")}>
+            //         Create
+            //     </Button>
+            // </Modal.Footer>
+//         </Fragment>
+// 	);
+// }
