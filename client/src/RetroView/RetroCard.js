854e280a70d53061d488e50ea1ca5f72fe12bf4a
Nate Rohweder <nrohweder@ebsco.com>
2020-04-09 20:55:54 -0400

    restyle cards, fix card deletion logic

--- client/src/RetroView/RetroCard.js
+++ client/src/RetroView/RetroCard.js
@@ -1,5 +1,6 @@
 import React, { useState ,useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Card from 'react-bootstrap/Card';
 import * as HttpClient from '../HttpClient';
 
@@ -13,6 +14,21 @@ const styles = {
     },
     button: {
         width: "45px"
+    },
+    cardBody: {
+        padding: "25px"
+    },
+    likes: {
+        fontFamily: 'Josefin Sans',
+        fontStyle: "italic",
+        color: "#28a745",
+        width: "25px",
+        position: "absolute",
+        top: -1,
+        right: 0,
+        borderLeft: "1px solid rgba(40,167,69,0.4)",
+        borderBottom: "1px solid rgba(40,167,69,0.4)",
+        borderRadius: "3px"
     }
 }
 
@@ -31,13 +47,15 @@ export default function RetroCard(props) {
 
 	return (
         <Card key={props.item.id} style={styles.item} bg='light'>
-            <Card.Header>{likeCount}</Card.Header>
-            <Card.Body>
+            <Card.Body style={styles.cardBody}>
+                <div style={styles.likes}>{likeCount}</div>
                 <Card.Text>{props.item.Value}</Card.Text>
             </Card.Body>
             <Card.Footer style={styles.cardFooter} className="text-muted">
-                <Button style={styles.button} variant="outline-info" size="sm" onClick={() => updateItemLikes(props.item, 1)}>+1</Button>
-                <Button style={styles.button} variant="outline-info" size="sm" onClick={() => updateItemLikes(props.item, -1)}>-1</Button>
+                <ButtonGroup>
+                    <Button style={styles.button} variant="outline-secondary" size="sm" onClick={() => updateItemLikes(props.item, -1)}>-1</Button>
+                    <Button style={styles.button} variant="outline-success" size="sm" onClick={() => updateItemLikes(props.item, 1)}>+1</Button>
+                </ButtonGroup>
                 <Button style={styles.button} variant="outline-danger" size="sm" onClick={() => props.removeItem(props.item.Id)}>✘</Button>
             </Card.Footer>
         </Card>
--- client/src/RetroView/RetroCategory.js
+++ client/src/RetroView/RetroCategory.js
@@ -45,12 +45,14 @@ export default function RetroView(props) {
         };
         HttpClient.sendPost('boarditem', newItem).then(data => {
             setItems([...items, data]);
+            setValue("");
         });
 
     };
 
     const removeItem = (id) => {
-        setItems(items.filter(item => item.id !== id))
+        HttpClient.sendDelete(`BoardItem/${id}`);
+        setItems(items.filter(item => item.id !== id));
     }
 
     useEffect(() => {
@@ -59,7 +61,7 @@ export default function RetroView(props) {
                 setItems(boardItems)
             }
         });
-    }, []);
+    }, [items]);
 
 	return (
         <div style={styles.categoryColumn}>
@@ -71,11 +73,7 @@ export default function RetroView(props) {
                 <FormControl value={value} onChange={(e) => handleInputChange(e)} type="text" placeholder="Add Item" />
             </InputGroup>
             <div style={styles.itemsContainer}>
-                {items.length >= 1 ? (
-                    items.map(item => (<RetroCard item={item} removeItem={removeItem} />
-                ))) :   
-                    <span>Could Not Load Items :(</span>
-                }
+                   {items.map(item => <RetroCard key={item.Id} item={item} removeItem={removeItem} />)}
             </div>
 
         </div>
